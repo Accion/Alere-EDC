142f223e538bd8292fd3035a5ee2aa19ec2a4a19
vikas.mendhe <vikas.mendhe@accionlab.com>
2013-05-22 13:59:38 +0530

    Added MvcApp

---
+++ MvcApp/MvcApp/Controllers/PatientController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApp.Models;
+
+namespace MvcApp.Controllers
+{
+    public class PatientController : Controller
+    {
+        private PatientDBContext db = new PatientDBContext();
+
+        //
+        // GET: /Patient/
+
+        public ActionResult Index()
+        {
+            return View(db.Patients.ToList());
+        }
+
+        //
+        // GET: /Patient/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        //
+        // GET: /Patient/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Patient/Create
+
+        [HttpPost]
+        public ActionResult Create(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patients.Add(patient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(patient);
+        }
+
+        //
+        // GET: /Patient/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        //
+        // POST: /Patient/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patient);
+        }
+
+        //
+        // GET: /Patient/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        //
+        // POST: /Patient/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Patient patient = db.Patients.Find(id);
+            db.Patients.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcApp/MvcApp/Models/Patient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcApp.Models
+{
+    public class Patient
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Group { get; set; }
+        public string Weight { get; set; }
+    }
+
+    public class PatientDBContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
